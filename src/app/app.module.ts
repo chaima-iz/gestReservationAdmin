85a82ca7d49e4bd8cd007f7d5112a0b1f3310abd
chaima-iz <shaymaeizwaghne.01@gmail.com>
2023-02-11 13:16:07 +0100

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { ReservationsComponent } from './pages/reservations/reservations.component';
+import { ListReservationsComponent } from './components/reservations/list-reservations/list-reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    SidebarComponent,
+    ReservationsComponent,
+    ListReservationsComponent
   ],
   imports: [
     BrowserModule,
